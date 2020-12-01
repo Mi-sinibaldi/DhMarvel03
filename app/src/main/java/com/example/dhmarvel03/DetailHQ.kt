758b03cc11b1855c97f28eee0b77530c7ba429cc
mi-sinibaldi <mia_araujo92@hotmail.com>
2020-12-01 10:10:35 -0300

    desenvolvimento dos layouts

---
+++ app/src/main/java/com/example/dhmarvel03/DetailHQ.kt
@@ -0,0 +1,11 @@
+package com.example.dhmarvel03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailHQ : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_h_q)
+    }
+}
--- app/src/main/java/com/example/dhmarvel03/MainActivity.kt
+++ app/src/main/java/com/example/dhmarvel03/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.dhmarvel03
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbarPrincipal)
+        toolbar.title = "Marvel"
+        setSupportActionBar(toolbar)
+
+        supportActionBar!!.setDisplayHomeAsUpEnabled(false)
     }
 }
